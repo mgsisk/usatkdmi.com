2e51485c23004dc2ce94bb965041e9227bd48d72
Michael Sisk <me@mgsisk.com>
2025-03-03 16:11:27 -0500

    Create astro.config.ts

---
+++ astro.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "astro/config";
+
+import sitemap from "@astrojs/sitemap";
+
+import mdx from "@astrojs/mdx";
+
+export default defineConfig({
+  site: "https://usatkdmi.com",
+  build: { assets: "srv" },
+  experimental: { responsiveImages: true, svg: true },
+  image: { experimentalLayout: "responsive" },
+  integrations: [mdx(), sitemap()],
+});
