3253e2a87994f1f178cbf4d226af0a727c4acb45
Michael Sisk <me@mgsisk.com>
2025-05-23 10:31:33 -0400

    Fine tune (#44)
    Add styling for dark mode map and replace webp images with avif.

--- astro.config.ts
+++ astro.config.ts
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 export default defineConfig({
   site: "https://usatkdmi.com",
   build: { assets: "srv" },
-  experimental: { responsiveImages: true, svg: true },
-  image: { experimentalLayout: "responsive" },
+  experimental: { responsiveImages: true },
+  image: { experimentalLayout: "constrained" },
   integrations: [mdx(), sitemap()],
 });
